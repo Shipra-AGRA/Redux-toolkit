bc83c154d8ff4095efb040f3a170de4b2ca1f6e9
Shipra-AGRA <shipraagrawal230@gmail.com>
2025-06-28 00:13:32 +0530

    understood redux using counter example

--- App.js
+++ App.js
@@ -1,12 +1,31 @@
 import React from "react"
 import ReactDOM from 'react-dom/client'
+import { Provider, useDispatch, useSelector } from "react-redux"
+import { store } from "./src/redux/store"
+import { decrement, increment } from "./src/features/counter/counterSlice"
 
 const App = () => {
+    const count = useSelector((state) => state.counter.value)
+    const dispatch = useDispatch()
+
+    function handleIncrement() {
+        dispatch(increment())
+    }
+    function hnadleDecrement() {
+        dispatch(decrement())
+    }
     return (
-        <div>Hello</div>
+        <div className="container">
+            <button onClick={handleIncrement}>+</button>
+            <p>Count: {count}</p>
+            <button onClick={hnadleDecrement}>-</button>
+        </div>
     )
 }
 export default App
 
 const root = ReactDOM.createRoot(document.getElementById("root"))
-root.render(<App />)
+root.render(
+    <Provider store={store}>
+        <App />
+    </Provider>)
