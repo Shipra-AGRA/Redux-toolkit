782a4535944a96d80ea852e95c8961331cca56dd
Shipra-AGRA <shipraagrawal230@gmail.com>
2025-06-29 23:43:45 +0530

    implemented counter logic using redux

--- App.js
+++ App.js
@@ -1,24 +1,40 @@
-import React from "react"
+import React, { useState } from "react"
 import ReactDOM from 'react-dom/client'
 import { Provider, useDispatch, useSelector } from "react-redux"
 import { store } from "./src/redux/store"
-import { decrement, increment } from "./src/features/counter/counterSlice"
+import { decrement, increment, incrementByAmount, reset } from "./src/features/counter/counterSlice"
 
 const App = () => {
+    const [amount,setAmount]=useState()
     const count = useSelector((state) => state.counter.value)
     const dispatch = useDispatch()
 
     function handleIncrement() {
         dispatch(increment())
     }
-    function hnadleDecrement() {
+    function handleDecrement() {
         dispatch(decrement())
+    }
+    function handleResetClick(){
+        dispatch(reset())
+    }
+    function handleIncrementByAmount(){
+        dispatch(incrementByAmount(amount))
     }
     return (
         <div className="container">
             <button onClick={handleIncrement}>+</button>
             <p>Count: {count}</p>
-            <button onClick={hnadleDecrement}>-</button>
+            <button onClick={handleDecrement}>-</button>
+            <br/><br/>
+            <button onClick={handleResetClick}>Reset</button>
+            <br/><br/>
+            <input 
+            type="number"
+            placeholder="Enter Amount"
+            value={amount}
+            onChange={(e)=>{setAmount(e.target.value)}}/>
+            <button onClick={handleIncrementByAmount}>Increment By Amount</button>
         </div>
     )
 }
